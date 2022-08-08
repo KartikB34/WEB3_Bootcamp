5439f210da9201cdae8254ccb9590f10b8f27b9d
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-08-08 12:56:43 +0530

    updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+import logo from '../assets/logo.png'
+import './Navbar.css'
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
 }
 
-export default Navbar
+const drawerWidth = 240;
+const navItems = ['Community', 'Internships', 'Jobs'];
+
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center'}}>
+      <Typography className='logo-img'  variant="h6" sx={{ my: 2 }}>
+        <img src={logo} alt='logo' />
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+
+        <button className='button drawer login'>Login</button>
+        <button className='button drawer register'>Register</button>
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box  sx={{ display: 'flex' }}>
+      <AppBar className='navbar' component="nav" sx={{background:'linear-gradient(90deg, rgba(255, 153, 51, 0.3) 0.76%, rgba(255, 255, 255, 0.27) 35.9%, rgba(0, 57, 121, 0.3) 52.96%, rgba(255, 255, 255, 0.27) 71.57%, rgba(19, 136, 8, 0.3) 100%), #F4F4F5;'}}>
+        <Toolbar>
+          <IconButton
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' }, color:'#000' }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            className='logo-img logo-padd'
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <img src={logo} alt='logo' />
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#000' }}>
+                {item}
+              </Button>
+            ))}
+            <button className='button login'>Login</button>
+            <button className='button register'>Register</button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        
+      </Box>
+    </Box>
+  );
+}
